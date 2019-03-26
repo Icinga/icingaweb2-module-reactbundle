f44e3d2bdb20790e9d4d9b921907d3f89e4206bc
Thomas Gelf <thomas@gelf.net>
2019-03-26 07:30:27 +0100

    make-release: fix sed

--- bin/make-release.sh
+++ bin/make-release.sh
@@ -29,7 +29,7 @@ find vendor/ -type f -name "*.php" \
  | grep -v '/test/' \
  | xargs git add -f
 find vendor/ -type f -name LICENSE | xargs git add -f
-sed -i '' "s/^Version:.*/Version: v$VERSION/" module.info
+sed -i "s/^Version:.*/Version: v$VERSION/" module.info
 git add module.info
 git commit -m "Version v$VERSION"
 
